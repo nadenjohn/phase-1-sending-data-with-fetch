99c066ba8a080b60014216f3bb4789b4289589be
John <nadenjohn@gmail.com>
2022-03-10 17:42:11 -0800

    commit tree 5f1dfbe50abbf26494dff718aea3f595dd404af1

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email){
+    const formData = {
+    name: `${name}`,
+    email: `${email}`,
+  };
+  
+    const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+ fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        return object;
+    })
+    .catch (function (error) {
+        alert("Unauthorized Access");
+        console.log(error.message);
+      });
+}
